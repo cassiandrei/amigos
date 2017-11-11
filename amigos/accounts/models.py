f1524cf9941abe35e0d3c96110daaef7f0214923
Lucas Ferreira da Silva <lferreira@inf.ufsm.br>
2017-11-11 01:58:25 -0200

    Criação dos models

--- amigos/accounts/models.py
+++ amigos/accounts/models.py
@@ -21,13 +21,16 @@ class User(AbstractBaseUser, PermissionsMixin):
         ], help_text='Um nome curto que será usado para identificá-lo de forma única na plataforma'
     )
     name = models.CharField('Nome', max_length=100, blank=True)
-    email = models.EmailField('E-mail', unique=True)
+    email = models.EmailField('E-mail', unique=True, blank=True, null=True)
     is_staff = models.BooleanField('Equipe', default=False)
     is_active = models.BooleanField('Ativo', default=True)
     date_joined = models.DateTimeField('Data de Entrada', auto_now_add=True)
 
+    # Token facebook
+    token_api = models.CharField('Token', max_length=300, null=False)
+
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email']
+    #REQUIRED_FIELDS = ['email']
 
     objects = UserManager()
 
@@ -43,3 +46,48 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def get_short_name(self):
         return str(self).split(" ")[0]
+
+# Relacoes entre amigos
+class Match(models.Model):
+    points = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    match = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Match"
+        verbose_name_plural = "Matches"
+
+# Tabelas dos gostos
+class Books(models.Model):
+    id_api = models.IntegerField()
+    name = models.CharField('Nome', max_length=300, null=False)
+
+class Games(models.Model):
+    id_api = models.IntegerField()
+    name = models.CharField('Nome', max_length=300, null=False)
+
+class Musics(models.Model):
+    id_api = models.IntegerField()
+    name = models.CharField('Nome', max_length=300, null=False)
+
+class Videos(models.Model):
+    id_api = models.IntegerField()
+    name = models.CharField('Nome', max_length=300, null=False)
+
+# Relações NxN
+class VideoUser(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    video = models.ForeignKey(Videos, on_delete=models.CASCADE)
+
+class GameUser(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    game = models.ForeignKey(Games, on_delete=models.CASCADE)
+
+class BookUser(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Books, on_delete=models.CASCADE)
+
+class MusicUser(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    music = models.ForeignKey(Musics, on_delete=models.CASCADE)
+
