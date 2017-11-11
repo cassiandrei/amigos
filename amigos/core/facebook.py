380fad4545e19bc768c96a42fa01701f797b2ce4
Rafael Trindade <rtrindade@inf.ufsm.br>
2017-11-11 03:16:04 -0200

    facebook api improved

--- amigos/core/facebook.py
+++ amigos/core/facebook.py
@@ -2,6 +2,24 @@ import requests, json
 
 GRAPH_URL = "http://graph.facebook.com/"
 ACCESS_TOKEN_FIELD = "access_token="
+CLIENT_ID = "1625366447519489"
+CLIENT_SECRET = "d94c27f2b5cf98b53a4b53488ace6161"
+SCOPE = "public_profile,email,user_about_me,user_actions.books,user_actions.music,user_actions.video,user_birthday,user_friends,user_likes"
+
+def url_to_login(url):
+    return "https://www.facebook.com/dialog/oauth?client_id={client_id}&redirect_uri={url}&scope={scope}".format(
+        client_id = CLIENT_ID, 
+        url = url, 
+        scope = SCOPE
+    )
+
+def url_to_token(url, code):
+    return "https://graph.facebook.com/oauth/access_token?client_id={client_id}&redirect_uri={url}&client_secret={client_secret}&code={code}".format(
+        client_id = CLIENT_ID,
+        url = url,
+        client_secret = CLIENT_SECRET,
+        code = code
+    )
 
 def get_user_info(token):
     requisition = requests.get(GRAPH_URL + "me?" + ACCESS_TOKEN_FIELD + token)
