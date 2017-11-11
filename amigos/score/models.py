00a5e0bfc0b07c0d8edb30963eff2888ec6c2526
Vinicius Mateus Dreifke <vdreifke@inf.ufsm.br>
2017-11-11 06:52:46 -0200

    iniciando solicitacao de amigos

--- amigos/score/models.py
+++ amigos/score/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from accounts.models import User, Match, MatchManager
 
 # Create your models here.
+
+class Friendship(models.Model):
+    points = models.IntegerField(default=0)
+    user_id = models.IntegerField(null=False)
+    match = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Match"
+        verbose_name_plural = "Matches"
+        ordering = ['-points']
+
+    objects = MatchManager()
