bfaf0a097248539bcedb3f3036d0c12588fc07d2
cassiano <cassiano@cassibook.inf.ufsm.br>
2017-11-11 04:05:00 -0200

    perfil

--- amigos/accounts/models.py
+++ amigos/accounts/models.py
@@ -2,6 +2,7 @@
 
 import re
 
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.core import validators
 from django.contrib.auth.models import AbstractBaseUser, UserManager, PermissionsMixin
@@ -20,16 +21,16 @@ class User(AbstractBaseUser, PermissionsMixin):
         ], help_text='Um nome curto que será usado para identificá-lo de forma única na plataforma'
     )
     name = models.CharField('Nome', max_length=100, blank=True)
-    email = models.EmailField('E-mail', unique=True, blank=True, null=True)
+    email = models.EmailField('E-mail', unique=True)
     is_staff = models.BooleanField('Equipe', default=False)
     is_active = models.BooleanField('Ativo', default=True)
     date_joined = models.DateTimeField('Data de Entrada', auto_now_add=True)
 
     # Token facebook
-    token_api = models.CharField('Token', max_length=300, null=False, unique=True)
+    token_api = models.CharField('Token', max_length=300, null=False)
 
     USERNAME_FIELD = 'username'
-    # REQUIRED_FIELDS = ['email']
+    REQUIRED_FIELDS = ['email']
 
     objects = UserManager()
 
@@ -51,15 +52,27 @@ class User(AbstractBaseUser, PermissionsMixin):
 class MatchManager(models.Manager):
     def getcreate(self, user, match):
         try:
-            match = Match.objects.get(user=user, match=match)
+            match = Match.objects.get(user_id=user.id, match_id=match.id)
         except ObjectDoesNotExist:
-            match = Match.objects.create(user=user, match=match)
+            match = Match.objects.create(user_id=user.id, match_id=match.id)
         return match
 
     def calc_points(self, user):
         all_users = User.objects.all()
+        user_books = BookUser.objects.filter(user=user)
         for current_user in all_users:
-            match = self.getcreate(user=user, match=current_user)
+            if current_user.id != user.id:
+                match = self.getcreate(user=user, match=current_user)
+                match_books = BookUser.objects.filter(user=current_user)
+                for booku in user_books:
+                    for bookm in match_books:
+                        if booku.book.id == bookm.book.id:
+                            match.points += 1
+                match.save()
+                print(match.points)
+
+        total_matches = Match.objects.filter(user_id=user.id)
+        return total_matches
 
 
 # Relacoes entre amigos
@@ -80,6 +93,9 @@ class Match(models.Model):
 class Books(models.Model):
     id_api = models.IntegerField()
     name = models.CharField('Nome', max_length=300, null=False)
+
+    def __str__(self):
+        return self.name
 
 
 class Games(models.Model):
